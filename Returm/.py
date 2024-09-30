9b07a970313bd2cabd365965539eb08f1978f971
lucassoaresfr <lucasclachack@gmail.com>
2024-09-30 09:26:59 -0300

    b

---
+++ Returm/.py
@@ -0,0 +1,5 @@
+texto = "coisas impossiveis de acontecer"
+
+print("total de palavras do texto:", len(texto))
+print("quantas vezes se repete a:", texto.count("a"))
+print("coisas em texto:", 'coisas' in texto)
